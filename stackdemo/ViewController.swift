cd38142e804fc82c4903856d1e9157f59c87e659
gajender <gajenderj@orientaloutsourcing.com>
2022-03-31 17:11:57 +0530

    project z

--- stackdemo/ViewController.swift
+++ stackdemo/ViewController.swift
@@ -1,4 +1,4 @@
-//
+  //
 //  ViewController.swift
 //  stackdemo
 //
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var backgroundPic: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textFieldSetup()
+        
+    }
+    func textFieldSetup(){
+        password.leftViewMode = .always
+        let imageView = UIImageView()
+        imageView.image=UIImage(named: "myapp-40-40")
+        let contentView=UIView()
+        contentView.addSubview(imageView)
+        
+        
+        //content.frame=CGRect(x: 5, y: 0, width: password.frame.height, height: password.frame.height)
+        password.leftView=contentView
     }
 
-
+    @IBAction func LogIn(_ sender: Any) {
+        backgroundPic.image=UIImage(named: "bg1")
+    }
+    
+    
 }
 
