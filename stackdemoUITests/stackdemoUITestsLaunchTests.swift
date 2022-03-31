cd2b2638aa5a86c48393c4042c6a58bffce473ee
gajender <gajenderj@orientaloutsourcing.com>
2022-03-31 09:28:13 +0530

    Initial Commit

---
+++ stackdemoUITests/stackdemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  stackdemoUITestsLaunchTests.swift
+//  stackdemoUITests
+//
+//  Created by Gajender Jangir on 31/03/22.
+//
+
+import XCTest
+
+class stackdemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
